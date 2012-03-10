d3689e909ab427652f5c304809a5936428523fdf
ukyo <ukyo.web@gmail.com>
2012-03-11 02:20:54 +0900

    Blob -> ArrayBuffer

--- src/zip.js
+++ src/zip.js
@@ -10,9 +10,6 @@ jz.zip = jz.zip || {};
 
 (function(window, jz){
 
-var BlobBuilder = jz.BlobBuilder;
-
-
 /**
  * @param {number} i index of a File.
  * @param {number} centralDirHeaderSize
@@ -20,7 +17,6 @@ var BlobBuilder = jz.BlobBuilder;
  * @return {Uint8Array}
  */
 function getEndCentDirHeader(i, centralDirHeaderSize, offset){
-	var bb = new BlobBuilder();
 	var view = new DataView(new ArrayBuffer(22));
 	view.setUint32(0, jz.zip.END_SIGNATURE, true);
 	view.setUint16(4, 0, true);
@@ -30,16 +26,7 @@ function getEndCentDirHeader(i, centralDirHeaderSize, offset){
 	view.setUint32(12, centralDirHeaderSize, true);
 	view.setUint32(16, offset, true);
 	view.setUint16(20, 0, true);
-	// bb.append(getUint(jz.zip.END_SIGNATURE)); //signature
-	// bb.append(getUshort(0)); //disknum
-	// bb.append(getUshort(0)); //startdisknum
-	// bb.append(getUshort(i)); //diskdirentry
-	// bb.append(getUshort(i)); //direntry
-	// bb.append(getUint(centralDirHeaderSize)); //dirsize
-	// bb.append(getUint(offset)); //startpos
-	// bb.append(getUshort(0)); //commentlen
-	bb.append(view.buffer);
-	return bb.getBlob();
+	return new Uint8Array(view.buffer);
 }
 
 
@@ -70,9 +57,8 @@ HeaderBuilder.prototype = {
 	 * @return {Uint8Array}
 	 */
 	_getCommonHeader: function(){
-		var bb = new BlobBuilder();
-		var view = new DataView(new ArrayBuffer(26));
-		var compsize = this.buffer.byteLength;
+		var view = new DataView(new ArrayBuffer(26)),
+			compsize = this.buffer.byteLength;
 		view.setUint16(0, 10, true);
 		view.setUint16(2, 0, true);
 		view.setUint16(4, this.deflateFlag, true);
@@ -83,117 +69,54 @@ HeaderBuilder.prototype = {
 		view.setUint32(18, compsize, true);
 		view.setUint16(22, this.filename.length, true);
 		view.setUint16(24, 0, true);
-		// bb.append(getUshort(10)); //needvar
-		// bb.append(getUshort(0)); //option
-		// bb.append(getUshort(this.deflateFlag)); //comptype
-		// bb.append(getUshort(getFileTime(this.date))); //filetime
-		// bb.append(getUshort(getFileDate(this.date))); //filedate
-		// bb.append(getUint(jz.algorithms.crc32(this.buffer))); //crc32
-		// bb.append(getUint(this.buffer.byteLength)); //compsize
-		// bb.append(getUint(this.buffer.byteLength)); //uncompsize
-		// bb.append(getUshort(this.filename.length)); //fnamelen
-		// bb.append(getUshort(0)); //extralen
-		bb.append(view.buffer);
-		return bb.getBlob();
 		return new Uint8Array(view.buffer);
 	},
 	
 	/**
-	 * @return {Blob}
+	 * @return {Uint8Array}
 	 */
 	getLocalFileHeader: function(){
 		if(this._cache.lf) return this._cache.lf;
 		
-		var bb = new BlobBuilder();
-		var view = new DataView(new ArrayBuffer(4 + this._commonHeader.length + this.filename.length));
-		var arr = new Uint8Array(view.buffer);
-		var offset = 0;
+		var view = new DataView(new ArrayBuffer(4 + this._commonHeader.length + this.filename.length)),
+			arr = new Uint8Array(view.buffer),
+			offset = 0;
 		
-		// view.setUint32(offset, jz.zip.LOCAL_FILE_SIGNATURE, true);
-		// offset += 4;
-		// arr.set(this._commonHeader, offset);
-		// offset += this._commonHeader.length;
-		// view.setString(offset, this.filename);
-		// bb.append(view.buffer);
-		
-		bb.append(getUint(jz.zip.LOCAL_FILE_SIGNATURE)); //signature
-		bb.append(this._commonHeader);
-		bb.append(this.filename);
-		
-		return this._cache.lf = bb.getBlob();
+		view.setUint32(offset, jz.zip.LOCAL_FILE_SIGNATURE, true); offset += 4;
+		arr.set(this._commonHeader, offset); offset += this._commonHeader.length;
+		view.setString(offset, this.filename);
+		return this._cache.lf = new Uint8Array(view.buffer);
 	},
 	
 	/**
-	 * @return {Blob}
+	 * @return {Uint8Array}
 	 */
 	getCentralDirHeader: function(){
 		if(this._cache.cd) return this._cache.cd;
 		
-		var bb = new BlobBuilder();
-		var view = new DataView(new Uint8Array(20 + this._commonHeader + this.filename));
-		var arr = new Uint8Array(view.buffer);
-		var offset = 0;
+		var view = new DataView(new ArrayBuffer(20 + this._commonHeader.length + this.filename.length)),
+			arr = new Uint8Array(view.buffer),
+			offset = 0;
 		
-		view.setUint32(0, jz.zip.CENTRAL_DIR_SIGNATURE, true);
-		offset += 4;
-		view.setUint16(offset, 0x14, true);
-		arr.set(this._commonHeader, offset);
-		offset += this._commonHeader.length;
-		view.setUint16(offset, 0, true);
-		offset += 2;
-		view.setUint16(offset, 0, true);
-		offset += 2;
-		view.setUint16(offset, 0, true);
-		offset += 2;
-		view.setUint32(offset, this.dirFlag, true);
-		offset += 4;
-		view.setUint32(offset, this.offset, true);
-		offset += 4;
+		view.setUint32(0, jz.zip.CENTRAL_DIR_SIGNATURE, true); offset += 4;
+		view.setUint16(offset, 0x14, true); offset += 2;
+		arr.set(this._commonHeader, offset); offset += this._commonHeader.length;
+		view.setUint16(offset, 0, true); offset += 2;
+		view.setUint16(offset, 0, true); offset += 2;
+		view.setUint16(offset, 0, true); offset += 2;
+		view.setUint32(offset, this.dirFlag, true); offset += 4;
+		view.setUint32(offset, this.offset, true); offset += 4;
 		view.setString(offset, this.filename);
-		bb.append(view.buffer);
-		
-		// bb.append(getUint(jz.zip.CENTRAL_DIR_SIGNATURE)); //signature
-		// bb.append(getUshort(0x14)); //madevar
-		// bb.append(this._commonHeader);
-		// bb.append(getUshort(0)); //commentlen
-		// bb.append(getUshort(0)); //disknum
-		// bb.append(getUshort(0)); //inattr
-		// bb.append(getUint(this.dirFlag)); //outattr
-		// bb.append(getUint(this.offset)); //offset
-		// bb.append(this.filename);
-		return this._cache.cd = bb.getBlob();
-		
+		return this._cache.cd = new Uint8Array(view.buffer);
 	},
 	
 	/**
 	 * @return {number}
 	 */
 	getAchiveLength: function(){
-		return this.getLocalFileHeader().size + this.buffer.byteLength;
+		return this.getLocalFileHeader().length + this.buffer.byteLength;
 	}
 };
-
-
-/**
- * Get a buffer of 16bit unsigned integer.
- * 
- * @param {number} i
- * @return {ArrayBuffer}
- */
-function getUshort(i){
-	return new Uint16Array([i]).buffer;
-}
-
-
-/**
- * Get a buffer of 32bit unsigned integer.
- * 
- * @param {number} i
- * @return {ArrayBuffer}
- */
-function getUint(i){
-	return new Uint32Array([i]).buffer;
-}
 
 
 /**
@@ -223,13 +146,15 @@ function getFileTime(date){
  * 
  * @param {Array} files
  * @param {number} level
- * @return {Blob}
+ * @return {ArrayBuffer}
  */
 jz.zip.compress = function(files, level){
 	var n = 0,
 		offset = 0,
 		achiveBb = new BlobBuilder(),
 		centralDirBb = new BlobBuilder(),
+		achiveArr = [],
+		centralDirArr = [],
 		date = new Date();
 	
 	level = level || 1;
@@ -262,10 +187,12 @@ jz.zip.compress = function(files, level){
 			buffer = jz.algorithms.deflate(buffer, _level);
 			isDeflate = true;
 		}
+		
 		hb = new HeaderBuilder(buffer, name, date, offset, isDir, isDeflate);
-		achiveBb.append(hb.getLocalFileHeader());
-		achiveBb.append(buffer);
-		centralDirBb.append(hb.getCentralDirHeader());
+		achiveArr.push(hb.getLocalFileHeader());
+		achiveArr.push(new Uint8Array(buffer));
+		centralDirArr.push(hb.getCentralDirHeader());
+		
 		offset += hb.getAchiveLength();
 		n++;
 		
@@ -280,12 +207,13 @@ jz.zip.compress = function(files, level){
 		compress(item, '');
 	});
 	
-	achiveBb.append(centralDirBb.getBlob());
-	achiveBb.append(getEndCentDirHeader(n, centralDirBb.getBlob().size, offset));
-	return achiveBb.getBlob('application/zip');
+	achiveArr = achiveArr.concat(centralDirArr);
+	achiveArr.push(getEndCentDirHeader(n, jz.utils.concatByteArrays(centralDirArr).length, offset));
+	
+	return jz.utils.concatByteArrays(achiveArr).buffer;
 };
 
-//shortcut
+//alias
 jz.zip.c = jz.zip.pack = jz.zip.compress;
 
 })(this, jz);
