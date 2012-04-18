d419e2077f133402a3096c33aabe1630ec764c8d
ukyo <ukyo.web@gmail.com>
2012-04-19 03:49:07 +0900

    async

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ test('test utility functions', function(){
 	same(new Uint8Array(jz.utils.stringToArrayBuffer('util')), ui8arr, 'string to arraybuffer');
 });
 
-test('test compress algorithm', function(){
+test('test jz.algorithms.deflate, jz.algorithms.inflate', function(){
 	var arr = [1, 1, 1, 1];
 	var ui8arr = new Uint8Array(arr);
 	
@@ -24,78 +24,84 @@ test('test compress algorithm', function(){
 	same(new Uint8Array(arr), ui8arr, 'hoge');
 });
 
-test('test gzip', function(){
-	var gzFileBuffer = jz.utils.loadFileBuffer('sample.txt.gz');
-	var fileBuffer = jz.utils.loadFileBuffer('sample.txt');
-	
-	var decompressed = jz.gz.decompress(gzFileBuffer, true);
-	same(new Uint8Array(decompressed), new Uint8Array(fileBuffer), 'decompress test');
-	
-	var compressed = jz.gz.compress(fileBuffer, 6, {fname: 'sample.txt'});
-	
-	ok(jz.gz.decompress(compressed, true), 'check checksum');
-});
-
-test('test zlib', function(){
+asyncTest('test jz.zlib.compress, jz.zlib.decompress', function(){
 	//this file is compressed.
 	//http://www.nicovideo.jp/watch/nm12945826
-	var swf = jz.utils.loadFileBuffer('nicowari.swf');
-	var zlibBytes = new Uint8Array(swf).subarray(8);
-	//decompressed file size
-	
-	var decompressed = jz.zlib.decompress(zlibBytes, true);
-	ok(decompressed, 'decompress test');
-	
-	var compressed = jz.zlib.compress(decompressed);
-	ok(jz.zlib.decompress(compressed, true), 'compress test');
+	jz.utils.load('nicowari.swf', function(swf){
+		var zlibBytes = new Uint8Array(swf).subarray(8);
+		start();
+		var decompressed = jz.zlib.decompress(zlibBytes, true);
+		ok(decompressed, 'decompress test');
+		start();
+		var compressed = jz.zlib.compress(decompressed);
+		ok(jz.zlib.decompress(compressed, true), 'compress test');
+		start();
+	});
 });
 
-asyncTest('test zip', function(){
-	var load = jz.utils.loadFileBuffer;
-	var zipsample = load('zipsample.zip');
-	
-	var unpacked = jz.zip.unpack(zipsample);
+asyncTest('test jz.gz.compress, jz.gz.decompress', function(){
+	jz.utils.load(['sample.txt.gz', 'sample.txt'], function(gzFileBuffer, fileBuffer){
+		var decompressed = jz.gz.decompress(gzFileBuffer, true);
+		same(new Uint8Array(decompressed), new Uint8Array(fileBuffer), 'decompress test');
+		start();
+		var compressed = jz.gz.compress(fileBuffer, 6, {fname: 'sample.txt'});
+		ok(jz.gz.decompress(compressed, true), 'check checksum');
+		start();
+	});
+});
+
+asyncTest('test jz.zip.pack, jz.zip.unpack(sync)', function(){
 	var aPath = 'zipsample/a.txt';
 	var bPath = 'zipsample/folder/b.txt';
-	var a = load(aPath);
-	var b = load(bPath);
-	var aView = new DataView(a);
-	var bView = new DataView(b);
-	var aStr = aView.getString(0, a.byteLength);
-	var bStr = bView.getString(0, b.byteLength);
-	var corrects = [aStr, bStr];
-
-	[aPath, bPath].forEach(function(v, i){
-		unpacked.getFileAsText(v, function(text){
-			equal(text.replace("\r\n", "\n"), corrects[i].replace("\r\n", "\n"), 'test unpack');
-			start();
-		});
-	});
+	jz.utils.load(['zipsample.zip', aPath, bPath], function(zipsample, a, b){
+		var unpacked = jz.zip.unpack(zipsample);
+		var aView = new DataView(a);
+		var bView = new DataView(b);
+		var aStr = aView.getString(0, a.byteLength);
+		var bStr = bView.getString(0, b.byteLength);
+		var corrects = [aStr, bStr];
 	
-	var packed = jz.zip.pack([
-		{name: 'zipsample', children: [
-			{name: 'a.txt', buffer: a},
-			{name: 'folder', children: [
-				{name: 'b.txt', buffer: b}
-			]}
-		]}
-	], 6, function(data){
-		console.log(data);
-	});
-	
-	var fr = new FileReader();
-	
-	fr.onload = function(e){
-		var unpacked = jz.zip.unpack(e.target.result);
 		[aPath, bPath].forEach(function(v, i){
 			unpacked.getFileAsText(v, function(text){
-				equal(text.replace("\r\n", "\n"), corrects[i].replace("\r\n", "\n"), 'test pack');
+				equal(text.replace("\r\n", "\n"), corrects[i].replace("\r\n", "\n"), 'test unpack');
 				start();
 			});
 		});
-	};
+		
+		var files = [
+			{name: 'zipsample', children: [
+				{name: 'a.txt', buffer: a},
+				{name: 'folder', children: [
+					{name: 'b.txt', buffer: b}
+				]}
+			]}
+		];
+		
+		var packed = jz.zip.pack({
+			files: files,
+			level: 6,
+			async: false
+		});
+		
+		var fr = new FileReader();
+		
+		fr.onload = function(e){
+			var unpacked = jz.zip.unpack(e.target.result);
+			[aPath, bPath].forEach(function(v, i){
+				unpacked.getFileAsText(v, function(text){
+					equal(text.replace("\r\n", "\n"), corrects[i].replace("\r\n", "\n"), 'test pack');
+					start();
+				});
+			});
+		};
+		
+		var bb = new jz.BlobBuilder();
+		bb.append(packed)
+		fr.readAsArrayBuffer(bb.getBlob(packed));
+	});
 	
-	var bb = new jz.BlobBuilder();
-	bb.append(packed)
-	fr.readAsArrayBuffer(bb.getBlob(packed));
+});
+
+asyncTest('test jz.zip.pack, jz.zip.unpack(async)', function(){
+	start();
 });
