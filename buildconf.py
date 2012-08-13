4cc976ce7708b73de13237087db9987a5934d465
ukyo <ukyo.web@gmail.com>
2012-08-14 04:44:43 +0900

    Update buildconf.py

--- buildconf.py
+++ buildconf.py
@@ -1,20 +1,16 @@
+#conf file sample.
+#This sample makes to jz.zip.unpack
+
 #COMPILER_PATH = ""
 REPOSITORY_NAME = "jsziptools"
-REPOSITORY_URL = "http://github.com/ukyo/jsziptools" 
-OUTPUT_NAME = "jsziptools.min.js"
+REPOSITORY_URL = "http://github.com/ukyo/jsziptools"
+OUTPUT_NAME = "jsziptools.unzip.min.js"
 BUILD_PATH = "build"
 
 JS_FILES = (
     "src/jsziptools.js",
     "src/utils.js",
-    "src/algorithms/adler32.js",
     "src/algorithms/crc32.js",
-    "src/algorithms/deflate.js",
     "src/algorithms/inflate.js",
-    "src/zlib.compress.js",
-    "src/zlib.decompress.js",
-    "src/gz.compress.js",
-    "src/gz.decompress.js",
-    "src/zip.pack.js",
     "src/zip.unpack.js",
 )
