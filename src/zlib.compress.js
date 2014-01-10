8a6af9c10f9d042c11b52bfb43ca0bfb6186bae1
ukyo <ukyo.web@gmail.com>
2014-01-10 23:55:39 +0900

    comment, params

--- src/zlib.compress.js
+++ src/zlib.compress.js
@@ -9,10 +9,11 @@ utils.js
 /**
  * Compress to a zlib format buffer.
  * 
- * @param {ArrayBuffer|Uint8Array|Array|string} bytes
- * @param {integer} level compress level.
- * @return {ArrayBuffer} zlib format buffer.
+ * @param {ArrayBuffer|Uint8Array|Array|string} buffer
+ * @param {integer} level compression level.
+ * @return {Uint8Array}
  */
-expose('jz.zlib.compress', function (bytes, level, chunkSize) {
-  return zlib['deflate'](utils.toBytes(bytes), level, chunkSize).buffer;
+expose('jz.zlib.compress', function (buffer, level, chunkSize) {
+  var params = utils.getParams(arguments, ['buffer', 'level', 'chunkSize']);
+  return zlib['deflate'](utils.toBytes(params.buffer), params.level, params.chunkSize);
 });
