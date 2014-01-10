9d5c45881218f967d11a600b7ca767c132317276
ukyo <ukyo.web@gmail.com>
2014-01-10 23:42:01 +0900

    update

--- src/gz.decompress.js
+++ src/gz.decompress.js
@@ -1,64 +1,22 @@
-/* require:
-jsziptools.js
-utils.js
-crc32.js
-inflate.js
-*/
-
-
 /**
  * Decompress from a gzip format buffer.
  * 
- * @param {ArrayBuffer|Uint8Array|Array|string} bytes
+ * @param {ArrayBuffer|Uint8Array|Array|string} buffer
  * @param {boolean} check If check crc32 checksum, set true.
- * @return {ArrayBuffer}
+ * @return {Uint8Array}
  */
-gz.decompress = function(bytes, check){
-    var ret = {}, flg, offset = 10, checksum, view;
-    bytes = utils.toBytes(bytes);
-    view = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
-    
-    if(bytes[0] !== 0x1F || bytes[1] !== 0x8B) throw 'Error: invalid gzip file.';
-    if(bytes[2] !== 0x8) throw 'Error: not deflate.';
-    
-    flg = bytes[3];
-    
-    ret.mtime = view.getUint32(4, true);
-    
-    //fextra
-    if(flg & 0x4) {
-        offset += view.getUint16(offset, true) + 2;
-    }
-    
-    //fname
-    if(flg & 0x8) {
-        ret.fname = '';
-        while(bytes[offset] !== 0) ret.fname += String.fromCharCode(bytes[offset++]);
-        ++offset;
-    }
-    
-    //fcomment
-    if(flg & 0x10) {
-        ret.fcomment = '';
-        while(bytes[offset] !== 0) ret.fcomment += String.fromCharCode(bytes[offset++]);
-        ++offset;
-    }
-    
-    //fhcrc
-    if(flg & 0x2) {
-        offset += 2;
-    }
-    
-    ret = algorithms.inflate(bytes.subarray(offset, bytes.length - 8));
-    
-    if(check) {
-        checksum = view.getUint32(bytes.length - 8, true);
-        if(checksum !== algorithms.crc32(ret)) {
-            throw new Error('file is broken.');
-        }
-    }
-    
-    return ret;
-};
+gz.decompress = function (buffer, chunkSize) {
+    var params = utils.getParams(arguments, ['buffer', 'chunkSize']),
+        chunks = [];
+    stream.gz.decompress({
+        buffer: params.buffer,
+        streamFn: function (chunk) {
+            chunks.push(chunk);
+        },
+        shareMemory: false,
+        chunkSize: params.chunkSize
+    });
+    return utils.concatBytes(chunks);
+}
 
 expose('jz.gz.decompress', gz.decompress);
