2f1da44ce913fac291965862b6067dcbeb7a73dc
ukyo <ukyo.web@gmail.com>
2014-01-10 23:53:49 +0900

    add comments. params

--- src/stream/zlib.compress.js
+++ src/stream/zlib.compress.js
@@ -1,5 +1,10 @@
 /**
- * @param  {object} params
+ * @param {Array} files
+ * @param {function(chunk: Uint8Array)} streamFn
+ * @param {number} level - optional (default is `6`)
+ * @param {boolean} shareMemory - optional (default is `false`)
+ * @param {number} chunkSize - optional (default is `0x8000`)
+ * @return {Promise}
  * @example
  * jz.stream.zlib.compress({
  *   buffer: inputBuffer,
@@ -10,7 +15,8 @@
  *   chunkSize: 0xf000
  * });
  */
-stream.zlib.compress = function (params) {
+stream.zlib.compress = function (buffer, streamFn, level, shareMemory, chunkSize) {
+    var params = utils.getParams(arguments, ['buffer', 'streamFn', 'level', 'shareMemory', 'chunkSize']);
     params.input = utils.toBytes(params.buffer);
     zlib.stream.deflate(params);
 };
