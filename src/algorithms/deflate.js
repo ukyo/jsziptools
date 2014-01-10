770073ba37da1d4ae2846276cb70ccd5590fb8e7
ukyo <ukyo.web@gmail.com>
2014-01-10 23:33:04 +0900

    return Uint8Array

--- src/algorithms/deflate.js
+++ src/algorithms/deflate.js
@@ -5,12 +5,13 @@ utils.js
 
 
 /**
- * @param {ArrayBuffer|Uint8Array|Array|string} bytes
+ * @param {ArrayBuffer|Uint8Array|Array|string} buffer
  * @param {number} level
- * @return {ArrayBuffer}
+ * @return {Uint8Array}
  */
-algorithms.deflate = function (bytes, level, chunkSize) {
-  return zlib['rawDeflate'](utils.toBytes(bytes), level, chunkSize).buffer;
+algorithms.deflate = function (buffer, level, chunkSize) {
+    var params = utils.getParams(arguments, ['buffer', 'level', 'chunkSize']);
+    return zlib['rawDeflate'](utils.toBytes(params.buffer), params.level, params.chunkSize);
 };
 
 expose('jz.algorithms.deflate', algorithms.deflate);
